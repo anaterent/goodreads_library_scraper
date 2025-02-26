9bfdf444225985820a6d342a640eac6d76ba54f5
apricot <just.anastasia.terenteva@gmail.com>
2025-02-26 15:21:32 +1100

    filter functionality fixed

--- static/filterAvailable.js
+++ static/filterAvailable.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
             // Check each availability item within the current book item
             $(this).find(".availability-item").each(function () {
                 let status = $(this).data("status"); // This will get the string status
+                console.log(status)
 
                 // Check if the status is "Available"
                 if (status && status.includes("Available")) {
@@ -17,9 +18,9 @@ $(document).ready(function () {
 
             // Show or hide the book item based on availability
             if (onlyAvailable && !isAvailable) {
-                $(this).hide();
+                $(this).addClass("hidden");
             } else {
-                $(this).show();
+                $(this).removeClass("hidden");
             }
         });
     });
